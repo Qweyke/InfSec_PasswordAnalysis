71de4160af1d352eb1dd5b54ca1912d0b8da823a
Qweyke <kettenkrad1337@yandex.ru>
2025-02-06 21:13:49 +0500

    init commit

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <Windows.h>
+#include <iostream>
+
+uint32_t checkPswdStrength(std::string pswd)
+{
+	return 0;
+}
+
+uint32_t countHackTime(uint32_t speed, uint32_t tries, uint32_t lockTime)
+{
+	return 0;
+}
+
+int main()
+{
+	UINT ecoding = GetConsoleOutputCP();
+	std::cout << ecoding << "\n";
+	char inp;
+	std::cin >> inp;
+	return 0;
+}

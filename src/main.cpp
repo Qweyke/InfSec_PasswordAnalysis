f7daaac3fa89980c5145b05291fbff889ab79b0d
Qweyke <qweyke@gmail.com>
2025-02-10 17:16:24 +0500

    formatting fixed

--- src/main.cpp
+++ src/main.cpp
@@ -2,33 +2,42 @@
 #include <Windows.h>
 #include <iostream>
 
-uint32_t checkPswdStrength(const std::string &pswd)
-{	
+uint32_t handleMaskBits()
+{
+}
+
+uint32_t checkPswdStrength(const std::string& pswd)
+{
 	UINT encoding = GetConsoleOutputCP();
-	if (encoding == 866) {
-		std::cout << "DOS Cyrillic Russian - 866" << "\n";
+	if (encoding == 866)
+	{
+		std::cout << "DOS Cyrillic Russian - 866"
+		          << "\n";
 		uint32_t pswdPower = 0;
 
-		
+		uint32_t mask = 0;
 
-		for (char ch : pswd) {
-			if (ch >= 33 && ch < 48) {
+		for (char ch : pswd)
+		{
+			if (ch >= 33 && ch < 48)
+			{
 				pswdPower += 15;
 			}
 
-			else if (ch >= 48 && ch < 58) {
+			else if (ch >= 48 && ch < 58)
+			{
 				pswdPower += 10;
 			}
 
-			else if (ch >= 48 && ch < 58) {
+			else if (ch >= 48 && ch < 58)
+			{
 				pswdPower += 10;
 			}
 		}
-
-
 	}
 
-	else {
+	else
+	{
 		std::cout << "Encoding: " << encoding << "\n";
 		return 0;
 	}

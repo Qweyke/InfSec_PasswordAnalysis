9ce2c0d86769f56e3cf5cc56e8cebbccf4ab6068
Qweyke <qweyke@gmail.com>
2025-02-10 11:54:20 +0500

    first implementY

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,37 @@
 #include <Windows.h>
 #include <iostream>
 
-uint32_t checkPswdStrength(std::string pswd)
-{
+uint32_t checkPswdStrength(const std::string &pswd)
+{	
+	UINT encoding = GetConsoleOutputCP();
+	if (encoding == 866) {
+		std::cout << "DOS Cyrillic Russian - 866" << "\n";
+		uint32_t pswdPower = 0;
+
+		
+
+		for (char ch : pswd) {
+			if (ch >= 33 && ch < 48) {
+				pswdPower += 15;
+			}
+
+			else if (ch >= 48 && ch < 58) {
+				pswdPower += 10;
+			}
+
+			else if (ch >= 48 && ch < 58) {
+				pswdPower += 10;
+			}
+		}
+
+
+	}
+
+	else {
+		std::cout << "Encoding: " << encoding << "\n";
+		return 0;
+	}
+
 	return 0;
 }
 
@@ -14,9 +43,8 @@ uint32_t countHackTime(uint32_t speed, uint32_t tries, uint32_t lockTime)
 
 int main()
 {
-	UINT ecoding = GetConsoleOutputCP();
-	std::cout << ecoding << "\n";
-	char inp;
-	std::cin >> inp;
+	// std::string input;
+	// std::cin >> input;
+
 	return 0;
 }
